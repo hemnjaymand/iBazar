18b57406dbae6fdc17533f19686bd6c81f3e0d48
himan <himanjaymand@gmail.com>
2026-08-11 22:35:34 +0330

    fix: import enums

--- src/features/admin/services/create-notification.service.ts
+++ src/features/admin/services/create-notification.service.ts
@@ -1,7 +1,7 @@
 // features/system/services/create-notification.service.ts
 import { toNotificationDTO } from "@/features/system/lib/notification.mapper";
 import { notificationRepository } from "../repositories/notification.repository";
-import type { NotificationType } from "@prisma/client";
+import { NotificationType } from "@prisma/client/enums";
 
 export async function createNotificationService(userId: string, type: NotificationType, title: string, message: string) {
   const notification = await notificationRepository.create({ userId, type, title, message });
